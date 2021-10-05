4619ca65d6660690ecf2b484d85633d4639dd05b
Jane Doe <jane@example.com>
2021-10-05 19:40:05 -0300

    actualizo script

--- Script1/Scriptdescargadatos.R
+++ Script1/Scriptdescargadatos.R
@@ -1,2 +1,9 @@
 library(electorAr)
 show_available_elections(source = "data")
+library(readr)
+read_csv(file = "data/arg_presi_gral2003.csv")
+library(haven)
+
+lupu <- read_dta(file = "https://www.noamlupu.com/argentina_ecological_data.dta")
+
+write.csv(x = lupu, file = "lupu.csv")
